9df64681a84dd17dba46d42c3e8ff2753361d2d8
Joel Löf <jlof@evolution.com>
2025-02-08 03:38:56 +0100

    refactor: clean up decoder

--- src/decoder/decoder.ts
+++ src/decoder/decoder.ts
@@ -1,27 +1,9 @@
 import * as macros from "../../macros" with { type: "macro" }
-import type { BufferMessage, StartMessage } from './worker'
+import type { StartMessage } from './worker'
 import { SafariDecodeWorker } from './worker-shim'
 
 const START_MESSAGE = { type: "start" } as const
 const STOP_MESSAGE = { type: "stop" } as const
-
-if (Promise.withResolvers === undefined) {
-  Promise.withResolvers = (() => {
-    let resolve
-    let reject
-    const promise = new Promise((resolve_, reject_) => {
-      resolve = resolve_
-      reject = reject_
-    })
-    return { promise, resolve, reject }
-  }) as any;
-}
-
-const ProcessorImport = {
-  started: false,
-  done: false,
-  ...Promise.withResolvers<void>(),
-}
 
 export class Decoder {
   constructor(
@@ -37,34 +19,14 @@ export class Decoder {
     this.node.port.postMessage(STOP_MESSAGE)
   }
 
-  static get loaded() {
-    return ProcessorImport.done
-  }
-
   static async addModule(context: AudioContext, moduleUrl?: string | URL): Promise<void> {
-    if (ProcessorImport.done) {
-      return
-    }
-    if (ProcessorImport.started === true) {
-      await ProcessorImport.promise
-      return
-    }
     const str = macros.minified("src/decoder/processor.ts")
     const url = moduleUrl ?? URL.createObjectURL(new Blob([str], { type: "application/javascript" }))
-    console.log(`[decoder] addModule ${url}`)
-    const promise = context.audioWorklet.addModule(url)
-    ProcessorImport.started = true
-    await promise.catch(ProcessorImport.reject)
-    ProcessorImport.done = true
-    ProcessorImport.resolve()
+    await context.audioWorklet.addModule(url)
   }
 
   static create(context: AudioContext, config: DecoderConfig = {}): Decoder | never {
     console.log("[decoder] create", config)
-    if (!Decoder.loaded) {
-      Decoder.addModule(context)
-      throw new Error("[decoder] cannot create decoder before processor module added")
-    }
     const str = macros.minified("src/decoder/worker.ts")
     const url = config.workerUrl ?? URL.createObjectURL(new Blob([str], { type: "application/javascript" }))
     const needsShim = globalThis.AudioDecoder === undefined
@@ -95,14 +57,13 @@ export class Decoder {
   }
 }
 
-
-interface DecoderConfig {
+export interface DecoderConfig {
   workerUrl?: string
   websocketUrl?: string
   /** @default 48_000 */
-  bitratePerChannel?: number
+  // bitratePerChannel?: number
   // values that work: 2_500 | 5_000 | 10_000 | 20_000 | 40_000 | 60_000
   // basically any multiple of 2_500 less than 60_000
   // or 120_000 for mono, i think
-  frameDuration?: number
+  // frameDuration?: number
 }
--- src/decoder/index.ts
+++ src/decoder/index.ts
@@ -1,38 +1,42 @@
-import { canStart } from '../tools'
 import { Decoder } from './decoder'
-
-log("Decoder.ts")
-
-function log(...args: any[]) {
-  console.log(...args)
-}
-
+import { start } from 'can-start-audio-context'
 main()
 async function main() {
-  const status = document.createElement("h2")
-  status.innerText = "Click anywhere to start"
-  document.body.appendChild(status)
-  await canStart()
-  status.innerText = "Started"
-
   const sampleRate = 48_000
 
-  const context = new AudioContext({ sampleRate, latencyHint: "playback" })
-  if (context.state !== "running") {
-    await context.resume()
-  }
-  await Decoder.addModule(context) //, window.location.pathname + "/processor.js");
-  const params = new URLSearchParams(`${window.location.search}&${window.location.hash.slice(1)}`)
+  const status = StatusDisplay()
+  status.update("Click To Start Audio Context")
+  const context = await start(new AudioContext({ sampleRate, latencyHint: "playback" }))
+
+  status.update("Adding Module")
+  await Decoder.addModule(context);
+
+  status.update("Creating Decoder")
   const decoder = Decoder.create(context, {
     websocketUrl: getWebSocketUrl()
   });
   decoder.node.connect(context.destination)
-  function getWebSocketUrl() {
-    const socketParam = params.get("socket")
-    if (socketParam === null) {
-      return undefined
-    }
-    return decodeURIComponent(socketParam).replaceAll("\"", "")
-  }
+
+  status.update("Done")
   return
+}
+
+function StatusDisplay() {
+  const status = document.createElement("h2")
+  status.innerText = "Click anywhere to start"
+  document.body.appendChild(status)
+  return {
+    update(newStatus: string) {
+      status.innerText = newStatus
+    }
+  }
+}
+
+function getWebSocketUrl() {
+  const params = new URLSearchParams(`${window.location.search}&${window.location.hash.slice(1)}`)
+  const socketParam = params.get("socket")
+  if (socketParam === null) {
+    return undefined
+  }
+  return decodeURIComponent(socketParam).replaceAll("\"", "")
 }
